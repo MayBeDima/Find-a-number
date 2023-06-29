99f89bd6d06c50dc671b0a4afc8ecadd790bfbec
MayBeDima <95147489+MayBeDima@users.noreply.github.com>
2023-06-29 11:58:09 +0300

    Update sound-mode.js

--- scripts-module/sound-mode.js
+++ scripts-module/sound-mode.js
@@ -11,15 +11,15 @@ export const soundMode = {
   },
 
   timerSound() {
-    if (!this.mute) this.setSound('../assets/sounds/timer.mp3');
+    if (!this.mute) this.setSound('assets/sounds/timer.mp3');
   },
 
   rightAnswerSound() {
-    if (!this.mute) this.setSound('../assets/sounds/rightAnswer.mp3');
+    if (!this.mute) this.setSound('assets/sounds/rightAnswer.mp3');
   },
 
   wrongAnwerSound() {
-    if (!this.mute) this.setSound('../assets/sounds/wrongAnwer.mp3');
+    if (!this.mute) this.setSound('assets/sounds/wrongAnwer.mp3');
   },
 
 }
